acd7334438366374978fa0a5ee848b6339b14b21
Vitalii Makoviak <vitalii_makoviak@epam.com>
2022-06-30 10:16:48 +0300

    Course-1: removed initial code to make review easier

--- alhorythms/test1.php
+++ alhorythms/test1.php
@@ -1,70 +1,5 @@
 <?php
 
-class SinglyLinkedListNode {
-    public $data;
-    public $next;
-
-    public function __construct($node_data)
-    {
-        $this->data = $node_data;
-        $this->next = null;
-    }
-}
-
-class SinglyLinkedList {
-    public $head;
-    public $tail;
-
-    public function __construct()
-    {
-        $this->head = null;
-        $this->tail = null;
-    }
-
-    public function insertNode($node_data)
-    {
-        $node = new SinglyLinkedListNode($node_data);
-
-        if (is_null($this->head)) {
-            $this->head = $node;
-        } else {
-            $this->tail->next = $node;
-        }
-
-        $this->tail = $node;
-    }
-}
-
-function printSinglyLinkedList($node, $sep, $fptr)
-{
-    while (!is_null($node)) {
-        fwrite($fptr, $node->data);
-
-        $node = $node->next;
-
-        if (!is_null($node)) {
-            fwrite($fptr, $sep);
-        }
-    }
-}
-
-/*
- * Complete the 'removeDuplicates' function below.
- *
- * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
- * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
- */
-
-/*
- * For your reference:
- *
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode next;
- * }
- *
- */
-
 function removeDuplicates(SinglyLinkedListNode $rootNode): SinglyLinkedListNode {
     $node = $rootNode;
 
@@ -79,5 +14,3 @@ function removeDuplicates(SinglyLinkedListNode $rootNode): SinglyLinkedListNode 
 
     return $rootNode;
 }
-
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
